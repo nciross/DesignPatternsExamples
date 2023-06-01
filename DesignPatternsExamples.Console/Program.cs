ba2c7afbeb0f37f2d80218e475048b47fd27ebc0
Mohamed NCIR <mohamed.ncir@outlook.com>
2023-06-01 18:50:35 +0200

    Add Factory Method

--- DesignPatternsExamples.Console/Program.cs
+++ DesignPatternsExamples.Console/Program.cs
@@ -2,8 +2,10 @@
 using DesignPatternsExamples.Console._01_Creational;
 
 Console.WriteLine("#Design Pattern");
-Console.WriteLine("#Creational");
-Console.WriteLine("#Singleton");
+Console.WriteLine("#Creational/Singleton");
 var oneObject = Singleton.Instance;
 var twoObject = Singleton.Instance;
-Console.WriteLine($"Create two object then compare references , result is :{Object.ReferenceEquals(oneObject, twoObject)} ");
+Console.WriteLine($"Create two object then compare references , result is :{Object.ReferenceEquals(oneObject, twoObject)} so is the same");
+
+var orderService = new OrderService(new ChronopostShippingFactory());
+orderService.SendOrder(44);
